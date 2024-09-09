65b9a5b36527147c73364ae190fa51aa45a6f3d8
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-09-09 22:57:20 +0300

    Create email templates (#4)
    * implemented Thymeleaf configuration
    
    * implemented email verification template

---
+++ src/main/java/pet_project/DiscussHub/config/ThymeleafConfiguration.java
@@ -0,0 +1,28 @@
+package pet_project.DiscussHub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfiguration {
+  @Bean
+  public ClassLoaderTemplateResolver templateResolver() {
+    ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setPrefix("templates/");
+    templateResolver.setTemplateMode("HTML5");
+    templateResolver.setSuffix(".html");
+    templateResolver.setCharacterEncoding("UTF-8");
+    templateResolver.setOrder(1);
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+
+    return templateEngine;
+  }
+}
---
+++ src/main/java/pet_project/DiscussHub/constant/EmailConstants.java
@@ -0,0 +1,7 @@
+package pet_project.DiscussHub.constant;
+
+public class EmailConstants {
+  private EmailConstants() {}
+
+  public static String VERIFY_EMAIL_TEMPLATE_NAME = "EmailVerificationTemplate";
+}
